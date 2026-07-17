a016defdfcf63dc21cc234c64146ddd5d5acf234
Night <169058503+Nightliqht@users.noreply.github.com>
2026-07-17 16:49:54 +0500

    added support for vertical screens to countdown

--- JS/countdown.js
+++ JS/countdown.js
@@ -17,8 +17,8 @@ var x = setInterval(function() {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // show resultes
-        document.getElementById("days").innerHTML = days + " days";
-        document.getElementById("hms").innerHTML = hours + " hours " + " | " + minutes + " minutes " + " | " + seconds + " seconds ";
+        document.getElementById("days").innerHTML = days + " D";
+        document.getElementById("hms").innerHTML = hours + " H " + " | " + minutes + " m " + " | " + seconds + " s ";
 
         // If the count down is finished, write some text
         if (distance < 0) {
