82d1632e333895e1a64adf9a6a244dfd47d7148e
Nightliqht <blend47.0@outlook.com>
2026-08-30 20:01:20 +0500

    gg

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,20 @@
 let time = document.getElementById("current-time");
+let date = document.getElementById("current-date");
+
 
 
 setInterval(() => 
 {
 let d = new Date();
-  time.innerHTML = d.toLocaleTimeString([] , {hour : "2-digit", minute : "2-digit" ,hour12 : false});
+  time.innerHTML = d.toLocaleTimeString([] , {hour : "2-digit", minute : "2-digit" ,hour12 : false,});
+}, 1000);  
+
+setInterval(() => 
+{
+let e = new Date();
+  var da = e.toLocaleDateString([] , {day: "2-digit", month: "short"});
+  var year = e.toLocaleDateString([] , {year: "numeric"});
+  var we = e.toLocaleDateString([] , {weekday: "long"});
+
+  date.innerHTML = `${da} ${year}, ${we}`;
 }, 1000);  
